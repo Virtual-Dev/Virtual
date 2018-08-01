4f90f492d1c07c0af11a3c96874eb03ecbd5e781
Virtual-Dev <virtual.developer.crypto@gmail.com>
2018-07-31 20:49:29 -0400

    Reset protocol updates to initial standards

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3753,7 +3753,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         uint64_t nNonce = 1;
         vRecv >> pfrom->nVersion >> pfrom->nServices >> nTime >> addrMe;
 
-        if ((pfrom->nVersion < MIN_PEER_PROTO_VERSION) || (nBestHeight >= 225000 && pfrom->nVersion < MIN_PEER_PROTO_VERSION_FORK1))
+        if (pfrom->nVersion < MIN_PEER_PROTO_VERSION)
         {
             // disconnect from peers older than this proto version
             LogPrintf("partner %s using obsolete version %i; disconnecting\n", pfrom->addr.ToString(), pfrom->nVersion);
